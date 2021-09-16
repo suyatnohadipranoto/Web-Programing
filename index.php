6238600dcc9e8f4ec5a604c93e30637f18da2f0b
suyatnohadipranoto <suyatno.hadi1985@gmail.com>
2021-09-16 10:42:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>TES GIT</title>
+
+        <div>    
+            <marquee bgcolor="red" width=100% ><h2><b style="color:white; ">Hello World !!! </b></h2></marquee>
+            </textarea>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <center { margin: 1;
+                position: absolute;
+                top:40%
+                left:45%
+                -ms-transform: transalate(-40%,-40%);
+                transform: trannsalate(-40%,-40%)
+        </div>
+
+</head>
+
+</html>
